415c2087676dd6d595f421e800a19c4e07210995
ANU7MADHAV <anumadhavan888@gmail.com>
2024-12-01 04:41:31 +0530

    Feat : added authentication apis for users

---
+++ backend/handlers/user.go
@@ -0,0 +1,14 @@
+package handlers
+
+import (
+	"fmt"
+	"uncademy-app/internals/data"
+)
+
+func Signup(input data.User) {
+	fmt.Println("hello world")
+}
+
+func Login(input data.Logininput) {
+	fmt.Println("Hello world")
+}
