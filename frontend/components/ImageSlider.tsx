1af5b2e5297607e99c892600e22a45e1aea11b14
ANU7MADHAV <anumadhavan888@gmail.com>
2024-12-14 03:53:09 +0530

    Feat : slides uploading feature implemented fully

--- frontend/components/ImageSlider.tsx
+++ frontend/components/ImageSlider.tsx
@@ -10,38 +10,27 @@ const ImageSlider = () => {
     const getSlides = async () => {
       try {
         const res = await axios.post("http://localhost:8080/v1/slides", {
-          metadata: "mera-data1",
+          metadata: "mera-data2",
         });
         const resData = res.data;
 
-        // Detailed logging to understand the response structure
-        console.log("Full response data:", resData);
-        console.log("Type of resData:", typeof resData);
-        console.log("Keys of resData:", Object.keys(resData));
-
         let urls: string[] = [];
 
-        // More flexible parsing
         if (Array.isArray(resData)) {
-          // If the entire response is an array of URLs
           urls = resData;
         } else if (resData.ImageURLs) {
-          // If ImageURLs exists
           urls = Array.isArray(resData.ImageURLs)
             ? resData.ImageURLs
             : [resData.ImageURLs];
         } else if (resData.imageUrls) {
-          // Alternative key name
           urls = Array.isArray(resData.imageUrls)
             ? resData.imageUrls
             : [resData.imageUrls];
         } else if (resData.response) {
-          // Another possible key
           urls = Array.isArray(resData.response)
             ? resData.response
             : [resData.response];
         } else {
-          // Attempt to parse as JSON if it's a string
           try {
             const parsedUrls = JSON.parse(resData);
             urls = Array.isArray(parsedUrls) ? parsedUrls : [parsedUrls];
@@ -60,7 +49,6 @@ const ImageSlider = () => {
     getSlides();
   }, []);
 
-  // Prepare images for the slider
   const images = imagesUrls.map((url) => ({ url }));
 
   console.log("images", images[0]?.url[1]);
@@ -69,8 +57,8 @@ const ImageSlider = () => {
     <div>
       {imagesUrls.length > 0 && (
         <SimpleImageSlider
-          width={896}
-          height={504}
+          width={900}
+          height={900}
           images={images}
           showBullets={true}
           showNavs={true}
