0ad69949294db6f4a555c84d943b9d86cc49f531
ANU7MADHAV <anumadhavan888@gmail.com>
2024-12-14 11:25:57 +0530

    Fix : remove react-image-slider

--- frontend/components/ImageSlider.tsx
+++ frontend/components/ImageSlider.tsx
@@ -1,11 +1,13 @@
 "use client";
 import axios from "axios";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import Image from "next/image";
 import React, { useEffect, useRef, useState } from "react";
-import SimpleImageSlider from "react-simple-image-slider";
 
 const ImageSlider = () => {
   const [imagesUrls, setImagesUrls] = useState<string[]>([]);
   const webSocketRef = useRef<WebSocket | null>(null);
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
 
   useEffect(() => {
     webSocketRef.current = new WebSocket("ws://localhost:8080/ws?id=user123");
@@ -77,24 +79,51 @@ const ImageSlider = () => {
   }, []);
 
   const images = imagesUrls.map((url) => ({ url }));
-  console.log("images", images);
+
+  const prevUrl = (): void => {
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + images.length) % images.length
+    );
+  };
+
+  const nextUrl = (): void => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
 
   useEffect(() => {
     if (webSocketRef.current?.readyState == webSocketRef.current?.OPEN) {
-      webSocketRef.current?.send(JSON.stringify(images));
+      webSocketRef.current?.send(JSON.stringify(images[currentIndex].url));
     }
   }, [images]);
 
   return (
     <div>
-      {imagesUrls.length > 0 && (
-        <SimpleImageSlider
-          width={900}
-          height={900}
-          images={images}
-          showBullets={true}
-          showNavs={true}
-        />
+      {images.length > 0 && (
+        <>
+          <div>
+            <button
+              className="absolute left-0 top-1/2 transform h-[459px] rounded-xl hover:bg-[#1a222f] mx-1 -mt-[10px] -translate-y-1/2 bg-[#111927] text-white p-2 group"
+              onClick={prevUrl}
+            >
+              <ChevronLeft className="text-gray-400 group-hover:text-white" />
+            </button>
+            <section className="flex justify-center">
+              <Image
+                src={images[currentIndex].url}
+                alt="image"
+                width={900}
+                height={900}
+              />
+            </section>
+
+            <button
+              className="absolute right-0 top-1/2 transform h-[459px] rounded-xl hover:bg-[#1a222f] mx-1 -mt-[10px] -translate-y-1/2 bg-[#111927] text-white p-2 group"
+              onClick={nextUrl}
+            >
+              <ChevronRight className="text-gray-400 group-hover:text-white" />
+            </button>
+          </div>
+        </>
       )}
     </div>
   );
