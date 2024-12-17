95f1e658a3345b512ea34ba09959ef98a1af2664
ANU7MADHAV <anumadhavan888@gmail.com>
2024-12-17 14:34:03 +0530

    Feat : realtime drawing implemented using excalidraw

--- backend/cmd/api/websocket.go
+++ backend/cmd/api/websocket.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
 )
+
+type StrokeData struct {
+	Elements []interface{} `json:"elements"`
+	AppState AppState      `json:"appState"`
+}
+
+type AppState struct {
+	Collaborators     interface{} `json:"collaborators,omitempty"`
+	ViewModelEnabled  bool        `json:"ViewModelEnabled,omitempty"`
+	Theme             string      `json:"theme,omitempty"`
+	CurrentItemColors string      `json:"currentItemColors,omitempty"`
+}
+
+type WebSocketMessage struct {
+	Type   string     `json:"type"`
+	STROKE StrokeData `json:"stroke"`
+}
 
 type Client struct {
 	Conn   *websocket.Conn
@@ -16,6 +34,7 @@ type Client struct {
 
 type WebSocketServer struct {
 	Client map[Client]bool
+	rooms  map[string]map[*Client]bool
 }
 
 var upgrade = websocket.Upgrader{
@@ -29,6 +48,7 @@ var upgrade = websocket.Upgrader{
 func NewWebSocketService() *WebSocketServer {
 	return &WebSocketServer{
 		Client: make(map[Client]bool),
+		rooms:  make(map[string]map[*Client]bool),
 	}
 }
 
@@ -56,39 +76,85 @@ func (app *Applications) WebsocketHandler(c *gin.Context) {
 		RoomId: roomId,
 	}
 
+	roomClients, exists := app.webSocket.rooms[roomId]
+
+	if !exists {
+		roomClients = make(map[*Client]bool)
+		app.webSocket.rooms[roomId] = roomClients
+	}
+
+	roomClients[client] = true
+
 	app.webSocket.Client[*client] = true
 
 	for {
-		messageType, message, err := connection.ReadMessage()
-
+		_, message, err := connection.ReadMessage()
 		if err != nil {
-			fmt.Println("err", err.Error())
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				app.logger.Printf("WebSocket read error: %v", err)
+			}
 			break
 		}
+
 		fmt.Println("message", string(message))
-		app.BroadcastMessage(client, messageType, message)
-		// go messageHandler(message)
+		if err := app.HandleStrokeMessage(client, message); err != nil {
+			app.logger.Printf("Error handling stroke message: %v", err)
+		}
+
 	}
 }
 
-func (app *Applications) BroadcastMessage(sender *Client, messageType int, message []byte) {
+func (app *Applications) HandleStrokeMessage(send *Client, message []byte) error {
+	var wsMessage WebSocketMessage
 
-	for client := range app.webSocket.Client {
-		fmt.Println("sender roomId", sender.RoomId)
+	if err := json.Unmarshal(message, &wsMessage); err != nil {
+		app.logger.Printf("Error parsing stroke message %v", err)
+		return err
+	}
 
-		if client.UserId == sender.UserId {
-			continue
+	if wsMessage.Type == "sendStroke" {
+		dummyAppState := wsMessage.STROKE.AppState
+		dummyAppState.Collaborators = []interface{}{}
+		dummyAppState.ViewModelEnabled = true
+
+		broadCasteMessage := WebSocketMessage{
+			Type: "strokeData",
+			STROKE: StrokeData{
+				Elements: wsMessage.STROKE.Elements,
+				AppState: dummyAppState,
+			},
 		}
 
-		if client.UserId != sender.RoomId {
-			continue
-		}
-		fmt.Println("message", string(message))
+		fmt.Println("broadcastMessage", broadCasteMessage)
 
-		if err := client.Conn.WriteMessage(messageType, message); err != nil {
-			fmt.Println("error", err.Error())
-			client.Conn.Close()
-			delete(app.webSocket.Client, client)
+		broadcastData, err := json.Marshal(broadCasteMessage)
+		if err != nil {
+			app.logger.Printf("Error marshaling broadcast message: %v", err)
+			return err
+		}
+
+		fmt.Println("client", send)
+
+		roomClients, exists := app.webSocket.rooms[send.RoomId]
+
+		fmt.Println("client", roomClients)
+
+		if !exists {
+			fmt.Println("error not exist")
+		}
+
+		for client := range roomClients {
+			if client.UserId == send.UserId {
+				continue
+			}
+
+			if err := client.Conn.WriteMessage(websocket.TextMessage, broadcastData); err != nil {
+				fmt.Println("error", err.Error())
+				client.Conn.Close()
+
+			}
+
 		}
 	}
+	return nil
 }
---
+++ uncademy-fe/src/pages/UserWhiteBoard.tsx
@@ -0,0 +1,11 @@
+import WhiteBoardSubscribe from "../components/WhiteBoardSubscribe";
+
+const UserWhiteBoard = () => {
+  return (
+    <div>
+      <WhiteBoardSubscribe />
+    </div>
+  );
+};
+
+export default UserWhiteBoard;
